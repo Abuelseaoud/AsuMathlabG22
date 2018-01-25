d2840f19a8fe7838a0b0a1a4f2c9817f228af05f
Eslammedhat22 <33896395+Eslammedhat22@users.noreply.github.com>
2018-01-25 19:58:53 +0200

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -135,6 +135,104 @@ int myfind(string& s, int pos)
 	return -1;
 }
 
+///////////////////////////////////////////////////Subtract the expression/////////////////////////////////////////
+int findOPs(string& s, int pos)        
+{
+	for (int i = pos; i < s.length(); i++)
+	if (s[i] == '+' || s[i] == '-' || s[i] == '*' || s[i] == '/' || s[i] == '^' || (s[i] == 's' && s[i + 1] == 'i' && s[i + 2] == 'n') || (s[i] == 'c' && s[i + 1] == 'o' && s[i + 2] == 's') || (s[i] == 's' && s[i + 1] == 'q' && s[i + 2] == 'r' && s[i + 3] == 't'))
+		return i;
+	return -1;
+}
+int findSpaceBefore(string& s, int pos)         
+{
+	for (int i = pos; i >= 0; i--)
+	if (s[i] == ' ' || s[i] == '[' || s[i] == ']' || s[i] == ';')
+		return i;
+	return -1;
+}
+int findSpaceAfter(string& s, int pos)         
+{
+	for (int i = pos; i < s.length(); i++)
+	if (s[i] == ' ' || s[i] == '[' || s[i] == ']' || s[i] == ';')
+		return i;
+	return -1;
+}
+void updateString(string& s1)                /*replace the part to send to alaa */
+{
+	string s;
+	trim(s1);
+	int pos = findOPs(s1, 0), pos1, pos2;
+	while (pos != -1)
+	{
+		pos1 = findSpaceBefore(s1, pos);
+		pos2 = findSpaceAfter(s1, pos);
+		s = s1.substr(pos1 + 1, pos2 - pos1 - 1);
+		// Alaa's fn
+		s1 = s1.replace(pos1 + 1, pos2 - pos1 - 1, "Medhat"); // replace medhat with  alaa's fn .sendstring()
+		pos = findOPs(s1, 0);
+	}
+}
+
+
+////////////////////////////////////////////// Modify the string to match the standard format \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+
+string modify(string& s)
+{
+	CMatrix A;
+	CMatrix B;
+	string s1, s2, s3;
+	int i;
+	int j;
+	int k;
+
+	while (s.find("] [") != -1 || s.find("],[") != -1)
+	{
+		i = s.find("] [");
+		k = s.find("],[");
+		if (i != -1)
+		{
+			s1 = s.substr(i + 2);
+		}
+		else if (k != -1)
+		{
+			s1 = s.substr(k + 2);
+		}
+		j = s1.find("]");
+		s1 = s1.substr(0, j + 1);
+		A.copy(s1);
+
+		if (i != -1)
+		{
+			s2 = s.substr(0, i + 1);
+		}
+		else if (k != -1)
+		{
+			s2 = s.substr(0, k + 1);
+		}
+		j = s2.rfind("[");
+		s2 = s2.substr(j);
+		B.copy(s2);
+
+		B.concatinate(A);
+		s = s.replace(j, s1.length() + s2.length() + 1, B.sendString());
+	}
+	while (s.find("[") != -1)
+	{
+		s.erase(s.find("["), 1);
+	}
+	while (s.find("]") != -1)
+	{
+		s.erase(s.find("]"), 1);
+	}
+	s = s + "]";
+	s = '[' + s;
+	if (s[1] == ' ') s.erase(1, 1);
+	return s;
+
+}
+
+
 /*
 int main(int argc, char*argv[])
 {
