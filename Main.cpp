a7b9d7e359290a9b0e28f24cc46f3ffb338b815a
IslamElMahdy <33972003+IslamElMahdy@users.noreply.github.com>
2018-01-29 14:17:29 +0200

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -7,17 +7,29 @@
 #include <fstream>
 #include <cstdio>
 #include <exception>
+#include <stack>
+#include <string.h>
+#include "math.h"
+#include <ctgmath>
 using namespace std;
 
 //global variables 
-string varName, Matrix1, Matrix2, Matrix3;
-char Operator;
 int NVar = 0;
-int MA,MB;
+int tempNVar = 0;
 //array of varibles names
-string * varibleNames = new string[20];
+string * varibleNames = new string[100];
 //array of Matrices
-CMatrix * Matrices = new CMatrix[20];
+CMatrix * Matrices = new CMatrix[100];
+const string  tempvaribleNames [20]= {"#","##","###","####","#####","######","#######","########","#########","##########","###########","############","#############","##############","##############",
+                                      "################","#################","##################","###################","####################"};
+//array of  temp Matrices
+CMatrix * tempMatrices = new CMatrix[20];
+const string functionsname []={ "sinh",	"cosh",	"tanh","coth","sech","csch",
+	                            "sin","cos","tan","cot","sec","csc",	
+                                "asin",	"acos","atan","acot","asec","acsc",	
+                                "sqrt","rand","eye","zeros","ones","log10","log","exp"};
+
+
 void trimend(string & x)
 {
 	for (int i = x.length() - 1; i<0; i--)
@@ -57,10 +69,10 @@ void trimSpace(string & x)
 
 
 /*  get varible index from  the array   */
-int index(string varName, string * varibleNames, int NVar)
+int index(string varName, const string * varibleNames, int NVar)
 {
 
-	for (int i = 0; i <= NVar; i++)
+	for (int i = 0; i < NVar; i++)
 	{
 
 
@@ -77,318 +89,95 @@ int index(string varName, string * varibleNames, int NVar)
 
 }
 
-
-/* operation string to operation code*/
-
-void operationMaking(string operation, string &destination, string& MatrixA, string& MatrixB, char &Operator)
+// Function to get weight of an operator. An operator with higher weight will have higher precedence.
+int GetOperatorWeight(char op)
 {
-	string code = operation;
-
-
-
-	string delimiter = ".\'/*-+";
-	// get the name of destination matrix
-	destination = code.substr(0, code.find('='));
-
-	for (int i = 0; i<6; i++)
+	int weight = -1;
+	switch(op)
 	{
-		if ((code.find(delimiter[i])>0) && (code.find(delimiter[i])<code.length()))
-		{
-			if (delimiter[i]=='.')
-			{
-				//get the type of operation
-				Operator = delimiter[i];
-				// get the name of matrix 1 it will contain a number 
-				MatrixA = code.substr(code.find('=') + 1, code.find('/') - code.find('=') - 1);
-				// get the name of matrix 2
-				MatrixB = code.substr((code.find('/')) + 1);
-				int pos = MatrixB.find(';');
-				if (pos>=0 && pos<code.length())
-				{
-					MatrixB.erase(pos, 1);
-				}
-				break;
-				
-			}
-			else if (delimiter[i]=='\'')
-			{
-				//get the type of operation
-				Operator = delimiter[i];
-				// get the name of matrix 1
-				MatrixA = code.substr(code.find('=') + 1, code.find(delimiter[i]) - code.find('=') - 1);
-				break;
-			}
-			else
-			{
-				//get the type of operation
-				Operator = delimiter[i];
+	case '(':
+        {weight = 0;
+        break;}
+    case '+':
+	case '-':
+		{weight = 1;
 
-				// get the name of matrix 1
-				MatrixA = code.substr(code.find('=') + 1, code.find(delimiter[i]) - code.find('=') - 1);
-
-				// get the name of matrix 2
-				MatrixB = code.substr((code.find(delimiter[i])) + 1);
-				int pos = MatrixB.find(';');
-				if (pos>=0 && pos<code.length())
-				{
-					MatrixB.erase(pos, 1);
-				}
-				break;
-			}
-
-		}
+		break;}
+	case '*':
+	case '/':
+    case '%':
+		{weight = 2;
+		break;}
+	case '^':
+	case '\'':
+		{weight = 3;
+		break;}
 	}
-	
-	
-	
+	return weight;
 }
 
-
-
-
-// fun convert string into small element to initialize CMAtrix
-string MatrixInitializingString(string code, string & destinationName)
+// Function to perform an operation and return output.
+int HasHigherPrecedence( string op1,  string op2)
 {
-	//get matrix Name
-	destinationName = code.substr(0, code.find('='));
-	// return matrix value
-	return code.substr(code.find('['), code.find(']') - code.find('[') + 1);
+	int op1Weight = GetOperatorWeight(op1[0]);
+	int op2Weight = GetOperatorWeight(op2[0]);
 
+	return op1Weight > op2Weight ?  1: 0;
 }
-/////////////////////////////////////////////////////////////////////////////////
-void excute(string operation)
+int Isoperator(char a)
 {
-	if ( (operation.find("[")>=0) && (operation.find("[")<operation.length() ) )
-	{
-		CMatrix x(MatrixInitializingString(operation, varName));
-		int k = index(varName, varibleNames, NVar);
-		if (k != -1)
-			Matrices[k] = x;
-		else
-		{
-
-			Matrices[NVar] = x;
-			trimSpace(varName);
-			varibleNames[NVar] = varName;
-			NVar=NVar+1;
-		}
-		
-
-		
-	}
-
-	else if ((operation.find("=")>=0) && (operation.find("=")<operation.length()))
-	{
-		/*trimSpace(operation);*/
-		operationMaking(operation, Matrix3, Matrix1, Matrix2, Operator);
-		int m;
-		operation=operation.substr(operation.find("=")+1);
-		for(int h=0;h<NVar;h++)
-		{
-			if(operation.find(varibleNames[h])!=-1)
-			{
-				if(varibleNames[h]==Matrix1){m=h;}
-				else{m=h; break;}
-
-			}
-		}
-		int M1 = index(Matrix1, varibleNames, NVar);
-		int M2 = m;
-		int M3 = index(Matrix3, varibleNames, NVar);
-		switch (Operator)
-		{
-		case '*':
-		{
-					if (M1 == -1 || M2 == -1) throw("Error:you try to make operation on undefine matrix");
-					else
-					{
-						CMatrix result = Matrices[M1] * Matrices[M2];
-
-						if (M3 != -1)
-							Matrices[M3] = result;
-						else
-						{
-
-							Matrices[NVar] = result;
-							trimSpace(Matrix3);
-							varibleNames[NVar] = Matrix3;
-							NVar++;
-						}
-
-						cout << result;
-						break;
-					}
-					
-
-		}
-
-		case '/':
-		{
-					if (M1 == -1 || M2 == -1) throw("Error:you try to make operation on undefine matrix");
-					if(Matrices[M2].getDeterminant()==0) {cout<<"error:can't measure div as a second Matrix is a singular Matrix "<<endl; break;}
-					else
-					{
-						CMatrix result = Matrices[M1] / Matrices[M2];
-						if (M3 != -1)
-							Matrices[M3] = result;
-						else
-						{
-
-							Matrices[NVar] = result;
-							trimSpace(Matrix3);
-							varibleNames[NVar] = Matrix3;
-							NVar++;
-						}
-
-						cout << result;
-						break;
-					}
-		}
-
-		case '-':
-		{
-					if (M1 == -1 || M2 == -1) throw("Error:you try to make operation on undefine matrix");
-					else
-					{
-						CMatrix result = Matrices[M1] - Matrices[M2];
-
-						if (M3 != -1)
-							Matrices[M3] = result;
-						else
-						{
-
-							Matrices[NVar] = result;
-							trimSpace(Matrix3);
-							varibleNames[NVar] = Matrix3;
-							NVar++;
-						}
-
-						cout << result;
-						break;
-					}
-		}
-
-		case '+':
-		{
-					
-					if (M1 == -1 || M2 == -1) throw("Error:you try to make operation on undefine matrix");
-					else
-					{
-						CMatrix result = Matrices[M1] + Matrices[M2];
-
-						if (M3 != -1)
-							Matrices[M3] = result;
-						else
-						{
-
-							Matrices[NVar] = result;
-							trimSpace(Matrix3);
-							varibleNames[NVar] = Matrix3;
-							NVar++;
-						}
-
-						cout << result;
-						break;
-					}
-		}
-
-		case '\'':
-		{
-
-					if (M1 == -1) throw("Error:you try to make operation on undefine matrix");
-					else
-					{
-						CMatrix result = Matrices[M1].getTranspose();
-
-						if (M3 != -1)
-							Matrices[M3] = result;
-						else
-						{
-
-							Matrices[NVar] = result;
-							trimSpace(Matrix3);
-							varibleNames[NVar] = Matrix3;
-							NVar++;
-						}
-
-						cout << result;
-						break;
-					}
-		}
-
-		case '.':
-		{
-
-					if ( M2 == -1) throw("Error:you try to make operation on undefine matrix");
-					else
-					{
-						char*v = new char[50];
-						strcpy(v, Matrix1.c_str());
-						double value = atof(v);
-						value = (value != 0) ? value : 1;
-						CMatrix result = Matrices[M2].elementDiv(value);
-						delete[]v;
-						if (M3 != -1)
-							Matrices[M3] = result;
-						else
-						{
-
-							Matrices[NVar] = result;
-							trimSpace(Matrix3);
-							varibleNames[NVar] = Matrix3;
-							NVar++;
-						}
-
-						cout << result;
-						break;
-					}
-		}
-		}
-
-	}
-cout<<endl<<endl<<endl;	
-
+	if(a=='+'||a=='-'||a=='*'||a=='/'||a=='%'||a=='^'||a=='\'')return 1;
+	else return 0;
 }
-
-
 
 
 int main(int argc, char*argv[])
 {
-			
+
+	
+	 
 	
 	
-		    try
-			{
 				string op;
 
 				if (argc == 1)
 				{
+					
 					while (1)
 					{
+						try{
+								cout<<">>";
+								getline(cin, op);
+								int m;
+								m = op.find("[");
+								if (m>=0 && m<op.length())
+								{
+									while (1)
+									{
+										m = op.find("]");
+										if (m<0 || m >= op.length())
+										{
+											string temp;
+											getline(cin, temp);
+											op = op + temp;
+										}
+										else
+										{
+											break;
+										}
+									}
+								}
+                                    
+								if(op.find("[")==-1) operationHandling(op);
 
-						getline(cin, op);
-						int m;
-						m = op.find("[");
-						if (m>=0 && m<op.length())
-						{
-							while (1)
-							{
-								m = op.find("]");
-								if (m<0 || m >= op.length())
-								{
-									string temp;
-									getline(cin, temp);
-									op = op + temp;
-								}
-								else
-								{
-									break;
-								}
+								else ;
+
+
 							}
-						}
-
-						excute(op);
+							catch( const char * error)
+								{
+									cout<< error<<endl;
+								}
 
 					}
 
@@ -420,22 +209,33 @@ int main(int argc, char*argv[])
 							}
 						}
 
-						if(op.find("[")==-1) {trimSpace(op); cout<<op<<endl;}
-						excute(op);
+						if(op.find("[")==-1) 
+								{
+									try{operationHandling(op);}
+									catch( const char * error)
+									{
+										cout<< error<<endl;
+									}
+								}	
+						else ;
 
 
 					}
 				}
 				
-			}
-			catch (char* error)
-			{
-				cout << error << endl;
-			}
+			
 
+			
 		
 
 	delete[] varibleNames;
 	delete[] Matrices;
+	delete[] tempMatrices;
+
+
+
+
+
+
 
 }
